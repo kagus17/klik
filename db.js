71bf839f66f5eae9d08b4b0fb720880f10d454e2
kagus17 <64460974+kagus17@users.noreply.github.com>
2025-04-09 10:12:38 +0200

    Update db.js

--- db.js
+++ db.js
@@ -16,7 +16,11 @@ const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  port: 3306,
+  ssl: {
+    ca: process.env.DB_SSL_CA,  // pobieramy certyfikat SSL z zmiennej środowiskowej
+  }
 });
 
 module.exports = pool.promise();
