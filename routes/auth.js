b4f77918dec68e0e658893636a953d5ff82097d4
kagus17 <64460974+kagus17@users.noreply.github.com>
2025-05-16 07:56:23 +0200

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -69,7 +69,7 @@ router.post('/forgot-password', async (req, res) => {
   });
 
   // Treść e-maila
-  const resetLink = `http://localhost:3000/reset-password.html?token=${token}`;
+  const resetLink = `https://my-klik-app-e9f0fcd4hhe4atbn.westeurope-01.azurewebsites.net/reset-password.html?token=${token}`;
   const mailOptions = {
     from: 'kliktester78@gmail.com',
     to: email,
