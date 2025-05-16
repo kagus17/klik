c97cc3a76aaefa3ab100202e815a680aa979c855
kagus17 <64460974+kagus17@users.noreply.github.com>
2025-05-16 08:48:41 +0200

    Update db.js

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
-const mysql = require('mysql2');
+/*const mysql = require('mysql2');
 
-/*const pool = mysql.createPool({
+const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
   password: '',
