662ca841eff9e8dbdfdd065cc246591e8882ff48
kagus17 <64460974+kagus17@users.noreply.github.com>
2025-05-27 09:53:00 +0200

    Update dbConfig.js

--- dbConfig.js
+++ dbConfig.js
@@ -1,6 +1,13 @@
+const fs = require('fs');
+const path = require('path');
+
 module.exports = {
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'gra1v1'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: 3306,
+  ssl: {
+    ca: fs.readFileSync(path.join(__dirname, 'certs', 'DigiCertGlobalRootCA.crt.pem'))
+  }
 };
