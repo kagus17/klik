f966be6a67652e719aab9f95f80948bbbccca56b
kagus17 <64460974+kagus17@users.noreply.github.com>
2025-05-27 09:51:09 +0200

    Update db.js

--- db.js
+++ db.js
@@ -3,17 +3,8 @@ const fs = require('fs');
 const path = require('path');
 const mysql = require('mysql2');
 require('dotenv').config(); // wczytuje plik .env
-
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: 3306,
-  ssl: {
-    ca: fs.readFileSync(path.join(__dirname, 'certs', 'DigiCertGlobalRootCA.crt.pem'))
-  }
-});
+const dbConfig = require('./dbConfig');
+const pool = mysql.createPool(dbConfig);
 
 module.exports = pool.promise();
 
