a40d682a54ceec76f472670117ed52ae158dd0ac
Karol <lachowski999@gmail.com>
2025-05-29 10:05:20 +0200

    punkty z sql

--- routes/leaderboard.js
+++ routes/leaderboard.js
@@ -6,7 +6,7 @@ const db = require('../db');
 router.get('/leaderboard', async (req, res) => {
   try {
     const [rows] = await db.query(
-      `SELECT player_name AS player, (matches * 100 - time_played * 2) AS points
+      `SELECT player_name AS player, (matches * (100-time_played)) AS points
        FROM results
        ORDER BY points DESC
        LIMIT 10`
