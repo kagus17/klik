b421206f64b8fba4671ad27dd21c5dcc5e555af4
Lucassol02 <167631947+Lucassol02@users.noreply.github.com>
2025-06-06 16:05:07 +0200

    Poprawka formularzu resetowania hasła.
    Naprawiono działanie przycisku Pokaż/ukryj hasło, który działał tylko lokalnie (na dysku). Teraz powinien on działać również na localhost.

--- public/reset.js
+++ public/reset.js
@@ -10,10 +10,33 @@
  * @module PasswordReset
  */
 
+//
+// Skrypt dla przycisku pokaż/ukryj hasło.
+//
+		document.addEventListener('DOMContentLoaded', function () {
+			const pokazHaslo = document.querySelector('.input-space a');
+			const haslo = document.getElementById("password");
+			
+			pokazHaslo.addEventListener('click', function(e) {
+				e.preventDefault();
+				if(haslo.type == 'password')
+				{
+					haslo.type = 'text';
+					this.innerHTML = "<img src='showDark.png' title='Ukryj hasło'>";
+				}
+				else
+				{
+					haslo.type = 'password';
+					this.innerHTML = "<img src='hideDark.png' title='Pokaż hasło'>";
+				}
+			});
+		});
+	
 /**
  * Token resetowania hasła pobrany z parametrów URL.
  * @type {string|null}
  */
+ 
     const urlParams = new URLSearchParams(window.location.search);
     const token = urlParams.get('token');
 
